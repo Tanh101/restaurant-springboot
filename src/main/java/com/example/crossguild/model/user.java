ca038ecb24127008fc7a6bef994027b1c2d66976
Van Tanh Ly <lyvantanh101@gmail.com>
2022-09-30 11:52:24 +0700

    create user model

--- src/main/java/com/example/crossguild/model/user.java
+++ src/main/java/com/example/crossguild/model/user.java
@@ -1,8 +1,43 @@
 package com.example.crossguild.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
 public class user {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 64)
+    private String username;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(nullable = false, length = 64)
+    private String firstName;
+
+    @Column(nullable = false, length = 64)
+    private String lastName;
+
+    @Column(nullable = false)
+    private int permission;
+
 }
