df80a8df4373e5eda7a8047aa7bc89354d947672
Van Tanh Ly <lyvantanh101@gmail.com>
2022-09-30 09:08:14 +0700

    initialize project

---
+++ src/main/java/com/example/crossguild/CrossguildApplication.java
@@ -0,0 +1,13 @@
+package com.example.crossguild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossguildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossguildApplication.class, args);
+	}
+
+}
